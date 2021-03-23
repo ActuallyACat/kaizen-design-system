caad8c09971b11c7e9a8172503b20954affca5da
actuallyacat <allanna.beaton@gmail.com>
2021-03-24 10:27:31 +1100

    ci: Added coverage report to Buildkite

--- .buildkite/scripts/test.sh
+++ .buildkite/scripts/test.sh
@@ -2,4 +2,9 @@
 set -e
 
 yarn install --frozen-lockfile
-yarn test --ci
+yarn test --ci --collect-coverage
+
+buildkite-agent artifact upload "coverage/*"
+  cat << EOF | buildkite-agent annotate --style "info"
+    Read the <a href="artifact://coverage/index.html">uploaded coverage report</a>
+  EOF
