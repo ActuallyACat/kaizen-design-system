4268d6219bb0742fd34bbce2b4f849ad39290ce8
actuallyacat <allanna.beaton@gmail.com>
2021-03-24 11:32:15 +1100

    Correct environment

--- .buildkite/scripts/test.sh
+++ .buildkite/scripts/test.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
+#!/usr/bin/env bash
 set -e
 
 yarn install --frozen-lockfile
 yarn test --ci --collect-coverage
 
 buildkite-agent artifact upload "coverage/*"
-  cat << EOF | buildkite-agent annotate --style "info"
-    Read the <a href="artifact://coverage/index.html">uploaded coverage report</a>
-  EOF
+
+cat << EOF | buildkite-agent annotate --style "info"
+  Read the <a href="artifact://coverage/index.html">uploaded coverage report</a>
+EOF
