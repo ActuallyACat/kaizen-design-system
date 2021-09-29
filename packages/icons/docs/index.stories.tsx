0237f674bcd2f53e372d4b1e803cfd240b5b73c5
ActuallyACat <allanna.beaton@gmail.com>
2021-09-29 20:21:48 +1000

    Add example story

---
+++ packages/icons/docs/index.stories.tsx
@@ -0,0 +1,16 @@
+import * as React from "react"
+import Academy from "../build/icons/Academy"
+
+export default {
+  title: "academy",
+  component: Academy,
+  parameters: {
+    docs: {},
+  }
+}
+
+export const AcademyIconDemo = () => (
+  <Academy title="This is a title"/>
+)
+
+AcademyIconDemo.storyName = "Demo of the academy icon"
